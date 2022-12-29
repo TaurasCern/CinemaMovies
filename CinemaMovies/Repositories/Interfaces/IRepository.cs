d78691586c98339032984ca579f95155bd25bceb
juojuk <finanside@gmail.com>
2022-12-29 20:48:45 +0200

    Repo_atnaujinimas

---
+++ CinemaMovies/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace CinemaMovies.Repositories.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        // CRUD
+        Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? filter = null);
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>>? filter, bool tracked = true);
+        Task<bool> ExistAsync(Expression<Func<TEntity, bool>> filter);
+
+        Task CreateAsync(TEntity entity);
+        Task RemoveAsync(TEntity entity);
+        Task SaveAsync();
+    }
+}

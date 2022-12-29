763a751a0b80788611d10ff90578093f924771a5
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2022-12-29 19:45:29 +0200

    Added Inital context and models

---
+++ CinemaMovies/Data/CinemaContext.cs
@@ -0,0 +1,39 @@
+﻿using CinemaMovies.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaMovies.Data
+{
+    public class CinemaContext : DbContext
+    {
+        public CinemaContext(DbContextOptions<CinemaContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<LocalUser> LocalUsers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Local user
+            modelBuilder.Entity<LocalUser>()
+                .HasKey(u => u.UserId);
+            // Movie
+            modelBuilder.Entity<Movie>()
+                .HasKey(m => m.MovieId);
+            // Review
+            modelBuilder.Entity<Review>()
+                .HasKey(r => r.ReviewId);
+            modelBuilder.Entity<Review>()
+                .HasOne(r => r.User)
+                .WithMany(u => u.Reviews)
+                .HasForeignKey(r => r.UserId);
+            modelBuilder.Entity<Review>()
+                .HasOne(r => r.Movie)
+                .WithMany(u => u.Reviews)
+                .HasForeignKey(r => r.MovieId);
+        }
+    }   
+}
---
+++ CinemaMovies/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace CinemaMovies.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public int Title { get; set; }
+        public int Description { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
